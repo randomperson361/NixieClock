c1b560854a15da358a0959e6cd261d95859fb351
Andrew McNeill <mcneill.andrew.d@gmail.com>
2020-04-26 18:08:31 -0500

    Defined pins and constants

--- NixieClock.ino
+++ NixieClock.ino
@@ -1,12 +1,54 @@
 #include "Arduino.h"
+
+// Define Arduino pins
+#define PIN_BCD_A 			 2
+#define PIN_BCD_B 			 3
+#define PIN_BCD_C 			 4
+#define PIN_BCD_D 			 5
+#define PIN_BUZZER 			 6
+#define PIN_PWM_SS 			 9
+#define PIN_RCT_SS 			10
+#define PIN_SPI_MOSI 		11
+#define PIN_SPI_MISO 		12
+#define PIN_SPI_CLK 		13
+#define PIN_BRIGHT_KNOB 	A5
+#define PIN_SET_ALM_TIME 	A4
+#define PIN_PREV_BUTTON 	A3
+#define PIN_ADJ_BUTTON 		A2
+#define PIN_NEXT_BUTTON 	A1
+#define PIN_ALM_ON_OFF 		A0
+
+// Define PWM outputs
+#define PWM_TUBE_0 			37
+#define PWM_TUBE_1 			33
+#define PWN_TUBE_2 			29
+#define PWM_TUBE_3 			13
+#define PWM_TUBE_4 			 9
+#define PWM_TUBE_5 			 5
+#define PWN_DOT_0 			35
+#define PWM_DOT_1 			31
+#define PWN_DOT_2 			11
+#define PWN_DOT_3 			 7
+#define PWM_LED_0 			13
+#define PWM_LED_1 			14
+#define PWM_LED_2 			15
+#define PWM_LED_3 			 5
+#define PWM_LED_4 			 4
+#define PWM_LED_5 			 3
+
+// Define constants
+#define NUM_TLC5974 		 2
+
+
+
 //The setup function is called once at startup of the sketch
 void setup()
 {
-// Add your initialization code here
+
 }
 
 // The loop function is called in an endless loop
 void loop()
 {
-//Add your repeated code here
+
 }
