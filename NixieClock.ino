997f37814832294ae604e2f3e59e37110c5b81e8
Andrew McNeill <mcneill.andrew.d@gmail.com>
2020-04-28 21:33:53 -0500

    Got clock demo functioning

--- NixieClock.ino
+++ NixieClock.ino
@@ -29,33 +29,12 @@ const uint8_t PWM_LED[]  = {13, 14, 15, 5, 6, 7};
 Adafruit_TLC5947 pwm = Adafruit_TLC5947(NUM_TLC5974, PIN_SPI_CLK, PIN_SPI_MOSI, PIN_PWM_SS);
 uint8_t loopCounter = 0;
 
-void printTime()
+void setTubeDigit(uint8_t value)
 {
-  Serial.print(String(rtc.hour()) + ":"); // Print hour
-  if (rtc.minute() < 10)
-    Serial.print('0'); // Print leading '0' for minute
-  Serial.print(String(rtc.minute()) + ":"); // Print minute
-  if (rtc.second() < 10)
-    Serial.print('0'); // Print leading '0' for second
-  Serial.print(String(rtc.second())); // Print second
-
-  if (rtc.is12Hour()) // If we're in 12-hour mode
-  {
-    // Use rtc.pm() to read the AM/PM state of the hour
-    if (rtc.pm()) Serial.print(" PM"); // Returns true if PM
-    else Serial.print(" AM");
-  }
-
-  Serial.print(" | ");
-
-  // Few options for printing the day, pick one:
-  Serial.print(rtc.dayStr()); // Print day string
-  //Serial.print(rtc.dayC()); // Print day character
-  //Serial.print(rtc.day()); // Print day integer (1-7, Sun-Sat)
-  Serial.print(" - ");
-  Serial.print(String(rtc.month()) + "/" +   // Print month
-                 String(rtc.date()) + "/");  // Print date
-  Serial.println(String(rtc.year()));        // Print year
+	for (int i = 0; i<4; i++)
+	{
+		digitalWrite(PIN_BCD[i], (value>>i) % 2);
+	}
 }
 
 //The setup function is called once at startup of the program
@@ -84,32 +63,58 @@ void setup()
 	// Uncomment to manually set time rtc.setTime(s, m, h, day, date, month, year)
 	// rtc.setTime(0, 4, 20, 2, 28, 4, 20);
 	rtc.update();
-
-	// Use the serial monitor to view time/date output
-	Serial.begin(9600);
 }
 
 // The loop function is called in an endless loop
 void loop()
 {
+	loopCounter++;
+	loopCounter = loopCounter % 10;
+
 	// RTC testing
 	static int8_t lastSecond = -1;
 	rtc.update();
 	if (rtc.second() != lastSecond) // If the second has changed
 	{
-	  printTime(); // Print the new time
-	  lastSecond = rtc.second(); // Update lastSecond value
+		lastSecond = rtc.second(); // Update lastSecond value
 	}
 
-	// Clear all PWM outputs
+	// Blank Tubes
 	for (int i=0; i<(24*NUM_TLC5974); i++)
 	{
 		pwm.setPWM(i, 0);
 	}
+	pwm.writeFaster();
+	delayMicroseconds(350); // blanking interval
+
+	// Setup digit value
+	switch (loopCounter)
+	{
+	case 0:
+		setTubeDigit(rtc.hour()/10);
+		break;
+	case 1:
+		setTubeDigit(rtc.hour()%10);
+		break;
+	case 2:
+		setTubeDigit(rtc.minute()/10);
+		break;
+	case 3:
+		setTubeDigit(rtc.minute()%10);
+		break;
+	case 4:
+		setTubeDigit(rtc.second()/10);
+		break;
+	case 5:
+		setTubeDigit(rtc.second()%10);
+		break;
+	default:
+		break;
+	}
+	delayMicroseconds(50);
+
 
 	// Loop through tubes lighting one at a time
-	loopCounter++;
-	loopCounter = loopCounter % 10;
 	if (loopCounter <= 5)
 	{
 		pwm.setPWM(PWM_TUBE[loopCounter], 4095);
@@ -127,5 +132,7 @@ void loop()
 	pwm.setLED(PWM_LED[4],0,40,0);
 	pwm.setLED(PWM_LED[5],0,0,40);
 
+	// Write to tubes
 	pwm.writeFaster();
+	delayMicroseconds(1000);
 }
