c9de115b8531b5df7afc6e0b1119325b9ee2047c
Andrew McNeill <mcneill.andrew.d@gmail.com>
2020-04-30 19:51:28 -0500

    Got inputs working

--- NixieClock.ino
+++ NixieClock.ino
@@ -12,10 +12,11 @@ const uint8_t PIN_BCD[] = {2, 3, 4, 5};
 #define PIN_SPI_CLK 		13
 #define PIN_BRIGHT_KNOB 	A5
 #define PIN_SET_ALM_TIME 	A4
-#define PIN_PREV_BUTTON 	A3
+#define PIN_DOWN_BUTTON 	A3
 #define PIN_ADJ_BUTTON 		A2
-#define PIN_NEXT_BUTTON 	A1
+#define PIN_UP_BUTTON 		A1
 #define PIN_ALM_ON_OFF 		A0
+#define BIT_MAX_10			1023
 
 // Define PWM outputs
 const uint8_t PWM_TUBE[] = {37, 33, 29, 13, 9, 5};
@@ -28,6 +29,53 @@ const uint8_t PWM_LED[]  = {13, 14, 15, 5, 6, 7};
 // Initialize globals
 Adafruit_TLC5947 pwm = Adafruit_TLC5947(NUM_TLC5974, PIN_SPI_CLK, PIN_SPI_MOSI, PIN_PWM_SS);
 uint8_t loopCounter = 0;
+bool buttonStateAdj = 0;
+bool buttonStateUp = 0;
+bool buttonStateDown = 0;
+bool switchAlarmOn = 0;
+uint16_t knobBright = 0;
+bool switchSetAlm = 0;
+bool switchSetTime = 0;
+
+void readInputs()
+{
+
+	buttonStateUp = !digitalRead(PIN_UP_BUTTON);
+	buttonStateAdj = !digitalRead(PIN_ADJ_BUTTON);
+	buttonStateDown = !digitalRead(PIN_DOWN_BUTTON);
+	switchAlarmOn = !digitalRead(PIN_ALM_ON_OFF);
+	knobBright = 4095 - 4*analogRead(PIN_BRIGHT_KNOB);
+	uint16_t slider = analogRead(PIN_SET_ALM_TIME);
+	switchSetAlm = 0;
+	switchSetTime = 0;
+	if (slider > (2*BIT_MAX_10)/3)
+	{
+		switchSetTime = 1;
+	}
+	else if (slider > BIT_MAX_10/3)
+	{
+		switchSetAlm = 1;
+	}
+}
+
+void printInputs()
+{
+	Serial.print("Button Up: ");
+	Serial.print(buttonStateUp);
+	Serial.print("\tButton Adj: \t");
+	Serial.print(buttonStateAdj);
+	Serial.print("\tButton Down: \t");
+	Serial.print(buttonStateDown);
+	Serial.print("\tSwitch Alm: \t");
+	Serial.print(switchAlarmOn);
+	Serial.print("\tKnob Bright: \t");
+	Serial.print(knobBright);
+	Serial.print("\tSwitch Alm: \t");
+	Serial.print(switchSetAlm);
+	Serial.print("\tSwitch Time: \t");
+	Serial.print(switchSetTime);
+	Serial.print("\n");
+}
 
 void setTubeDigit(uint8_t value)
 {
@@ -37,31 +85,42 @@ void setTubeDigit(uint8_t value)
 	}
 }
 
+void blankAll()
+{
+	for (int i=0; i<(24*NUM_TLC5974); i++)
+	{
+		pwm.setPWM(i, 0);
+	}
+	pwm.writeFaster();
+}
+
 //The setup function is called once at startup of the program
 void setup()
 {
+	Serial.begin(9600);
+
 	// Setup pins
 	for (int i = 0; i<4; i++)
 	{
 		pinMode(PIN_BCD[i], OUTPUT);
 		digitalWrite(PIN_BCD[i], LOW);
 	}
-	pinMode(PIN_PREV_BUTTON, INPUT_PULLUP);
+	pinMode(PIN_UP_BUTTON, INPUT_PULLUP);
 	pinMode(PIN_ADJ_BUTTON, INPUT_PULLUP);
-	pinMode(PIN_NEXT_BUTTON, INPUT_PULLUP);
+	pinMode(PIN_DOWN_BUTTON, INPUT_PULLUP);
+	pinMode(PIN_ALM_ON_OFF, INPUT);
+	pinMode(PIN_BRIGHT_KNOB, INPUT);
+	pinMode(PIN_ALM_ON_OFF, INPUT);
+	readInputs();
 
 	// Initialize PWM board to all off
 	pwm.begin();
-	for (int i=0; i<(24*NUM_TLC5974); i++)
-	{
-		pwm.setPWM(i, 0);
-	}
-	pwm.writeFaster();
+	blankAll();
 
 	// Initialize RTC
 	rtc.begin(PIN_RTC_SS);
 	// Uncomment to manually set time rtc.setTime(s, m, h, day, date, month, year)
-	// rtc.setTime(0, 4, 20, 2, 28, 4, 20);
+	//rtc.setTime(0, 49, 18, 4, 30, 4, 20);
 	rtc.update();
 }
 
@@ -71,6 +130,9 @@ void loop()
 	loopCounter++;
 	loopCounter = loopCounter % 10;
 
+	readInputs();
+	//printInputs();
+
 	// RTC testing
 	static int8_t lastSecond = -1;
 	rtc.update();
@@ -79,12 +141,7 @@ void loop()
 		lastSecond = rtc.second(); // Update lastSecond value
 	}
 
-	// Blank Tubes
-	for (int i=0; i<(24*NUM_TLC5974); i++)
-	{
-		pwm.setPWM(i, 0);
-	}
-	pwm.writeFaster();
+	blankAll();
 	delayMicroseconds(350); // blanking interval
 
 	// Setup digit value
@@ -111,17 +168,15 @@ void loop()
 	default:
 		break;
 	}
-	delayMicroseconds(50);
-
 
 	// Loop through tubes lighting one at a time
 	if (loopCounter <= 5)
 	{
-		pwm.setPWM(PWM_TUBE[loopCounter], 4095);
+		pwm.setPWM(PWM_TUBE[loopCounter], knobBright);
 	}
 	else
 	{
-		pwm.setPWM(PWM_DOT[loopCounter-6], 4095);
+		pwm.setPWM(PWM_DOT[loopCounter-6], knobBright);
 	}
 
 	// RGB pattern on LEDs for test
